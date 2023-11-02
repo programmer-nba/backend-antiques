c0e148d09e2c65dfc2076e36f82c5c6378ec2885
Progammer <programmer@nbadigitalworlds.com>
2023-11-02 10:37:54 +0700

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const connection = require("./config/db");
+connection();
+
+app.use(express.json());
+
+
+const port = process.env.PORT || 4002;
+app.listen(port, console.log(`Listening on port ${port}`));
